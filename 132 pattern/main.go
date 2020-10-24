f780a8d476f16064654931c72f48f03957f684fd
abit2 <ankit.baruah@onefootball.com>
2020-10-24 17:54:56 +0200

    day 4 leetcode

---
+++ 132 pattern/main.go
@@ -0,0 +1,63 @@
+package main
+
+func find132pattern(nums []int) bool {
+	if len(nums) < 3 {
+		return false
+	}
+
+	smaller := make([]int, len(nums))
+	for i, v := range nums {
+		if i == 0 {
+			smaller[i] = nums[i]
+			continue
+		}
+
+		if v < smaller[i-1] {
+			smaller[i] = v
+			continue
+		}
+		smaller[i] = smaller[i-1]
+	}
+
+	var b bool
+	for i, v := range nums {
+		if v > smaller[i] {
+			aa := make([]int, len(nums))
+			copy(aa, nums)
+			b = binarysearch(aa[i+1:], smaller, i, v)
+		}
+
+		if b == true {
+			return true
+		}
+	}
+	return false
+}
+
+func binarysearch(nums []int, smaller []int, index int, v2 int) bool {
+	sort.Slice(nums, func(ii, jj int) bool{
+		return nums[ii] < nums[jj]
+	})
+
+	v1 := smaller[index]
+
+	left, right := 0, len(nums)-1
+	for left <= right {
+		middle := left + (right-left)/2
+
+		if v1 < nums[middle] && v2 > nums[middle] {
+			return true
+		}
+
+		if v1 >= nums[middle] {
+			left = middle+1
+			continue
+		}
+
+		if v2 <= nums[middle] {
+			right = middle-1
+			continue
+		}
+	}
+	return false
+}
