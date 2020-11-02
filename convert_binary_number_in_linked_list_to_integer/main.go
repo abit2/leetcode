c9f67c30f5f7806ddb0d303ccb01a16f5102aa04
abit2 <ankit.baruah@onefootball.com>
2020-11-02 01:02:47 +0100

    day 9 leetcode

---
+++ convert_binary_number_in_linked_list_to_integer/main.go
@@ -0,0 +1,23 @@
+package main
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getDecimalValue(head *ListNode) int {
+	var s []int
+	for head != nil {
+		s = append([]int{head.Val}, s...)
+		head = head.Next
+	}
+
+	ans := 0
+	for i, v := range s {
+		if v == 1 {
+			ans += (1<<i)
+		}
+	}
+	return ans
+}
